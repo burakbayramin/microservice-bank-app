2d6c11b956425ae802b27173532fe30130181142
burakbayramin <burakbaydevs@gmail.com>
2024-02-10 16:17:06 +0300

    initial commit

---
+++ account/src/test/java/com/hrkhty/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrkhty.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
