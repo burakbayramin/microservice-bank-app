8ff83d9dcbd7c77d648358e000e159a1b173c88a
burakbayramin <burakbaydevs@gmail.com>
2024-02-19 01:35:42 +0300

    config server added

---
+++ configserver/src/test/java/com/hrkhty/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrkhty.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
