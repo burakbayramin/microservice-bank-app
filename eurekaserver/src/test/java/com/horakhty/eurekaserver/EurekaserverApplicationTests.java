8166426e3f418279f9f0dd7807d45440231dc68c
burakbayramin <burakbaydevs@gmail.com>
2024-02-28 15:42:37 +0300

    eureka server added not started

---
+++ eurekaserver/src/test/java/com/horakhty/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horakhty.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
